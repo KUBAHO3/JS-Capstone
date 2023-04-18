86d31101bc0a5b95fc719dfda9972a7e6cb44473
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-04-18 17:29:38 +0530

    Display data in landing page

--- src/module/displayList.js
+++ src/module/displayList.js
@@ -6,6 +6,27 @@ let url = `https://www.themealdb.com/api/json/v1/1/categories.php`
 const category = await fetchList(url);
 console.log("data: ",category.categories);
 
+const cardContainer = document.querySelector('.container-cards');
+cardContainer.innerHTML = ''
+const cards = category.categories.map((data) => `
+<div class="card">
+<img src=${data.strCategoryThumb} alt="The dish category">
+<div class="card-about">
+    <p class="card-name">
+        ${data.strCategory}
+    </p>
+    <div class="like">
+        <i class="fa-regular fa-heart"></i>
+        <span class="like-count">10 likes</span>
+    </div>
+</div>
+<div class="card-more">
+    <button type="button" class="card-button">Comments</button>
+    <button type="button" class="card-button">Reservations</button>
+</div>
+</div>
+`).join('');
+cardContainer.innerHTML = cards;
 
 };
 
