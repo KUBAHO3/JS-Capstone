d489dbfce986d6f54325883f46db64c3a269e3d7
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-04-18 17:25:32 +0530

    create display module & import it

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import './style.css';
 import addHeaderToPage from './module/addHeader.js';
+import displayList from './module/displayList.js';
 
 addHeaderToPage();
+displayList();
---
+++ src/module/displayList.js
@@ -0,0 +1,7 @@
+import fetchList from './fetchList.js';
+
+const displayList = async () => {
+
+};
+
+export default displayList;
