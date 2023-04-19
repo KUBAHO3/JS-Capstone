6a655dac70d17b90d1dc593ce287a15d01bf7d8c
Lawrence Amoafo <rekenburg777@gmail.com>
2023-04-19 10:24:00 +0000

    Configure function to display HTML elements in popup window
    Using the async/await property to fetch the API to display the element needed for the popup window

--- src/module/displayPopup.js
+++ src/module/displayPopup.js
@@ -1,12 +1,28 @@
+import fetchList from './fetchList.js';
+
 const openModal = document.querySelector('.modal');
 
-const popup = `<div class="modal-head">
-<div class="modal-image"></div>
-<button class="close-button">&times;</button>
+const Modal = async (id) => {
+  openModal.innerHTML = '';
+
+  const url = 'https://www.themealdb.com/api/json/v1/1/categories.php';
+
+  const category = await fetchList(url);
+
+  const rightElement = (idi) => {
+    const itemID = category.categories.filter((element) => element.idCategory === idi);
+    return itemID[0];
+  };
+
+  const data = rightElement(id);
+
+  const popup = `<div class="modal-head">
+<img src=${data.strCategoryThumb} alt="">
+<button class="close-button" data-close-button>&times;</button>
 </div>
 <div class="modal-content">
-<h2 class="modal-title"></h2>
-<p class="modal-description"></p>
+<h2 class="modal-title">${data.strCategory}</h2>
+<p class="modal-description">${data.strCategoryDescription}</p>
 <h3 class="comments">Comments<span class="no-of-comments"></span></h3>
 <ul class="comments-lists"></ul>
 <h4 class="add-comment">Add a comment</h4>
@@ -24,8 +40,7 @@ const popup = `<div class="modal-head">
 </div>
 `;
 
-const popupWindow = () => {
   openModal.insertAdjacentHTML('beforeend', popup);
 };
 
-export default popupWindow;
+export default Modal;
