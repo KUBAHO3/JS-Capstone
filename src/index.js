bc4dd92b4a86b4ee56681ed28380565784bc9234
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-04-19 18:13:55 +0530

    fix eslinter error

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import './style.css';
 import addHeaderToPage from './module/addHeader.js';
 import displayList from './module/displayList.js';
-import {  updateLikes } from './module/aboutLikes.js';
-
+import { updateLikes } from './module/aboutLikes.js';
 
 addHeaderToPage();
 displayList();
--- src/module/displayList.js
+++ src/module/displayList.js
@@ -1,15 +1,13 @@
 import fetchList from './fetchList.js';
 
-import { displayLike,  } from './aboutLikes.js';
+import { displayLike } from './aboutLikes.js';
 
 const displayList = async () => {
   const url = 'https://www.themealdb.com/api/json/v1/1/categories.php';
   const urli = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/gXvWpQoPOY3Cj1qr1TRg/likes';
 
-
   const category = await fetchList(url);
   const likesdata = await fetchList(urli);
-
 
   const cardContainer = document.querySelector('.container-cards');
 
@@ -33,8 +31,6 @@ const displayList = async () => {
 </div>
 `).join('');
   cardContainer.innerHTML = cards;
-  
-  
 };
 
 export default displayList;
