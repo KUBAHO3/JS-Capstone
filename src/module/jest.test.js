53f5d488a43dc085f0c7671f2a3dc3d4a87c0499
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-04-20 12:25:06 +0530

    add arrange pattern

--- src/module/jest.test.js
+++ src/module/jest.test.js
@@ -1,1 +1,16 @@
 import countItems from './countItems.js';
+
+describe('countItems', () => {
+  beforeEach(() => {
+    document.body.innerHTML = `
+      <div class="container-cards">
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+      </div>
+      <h2 id="categoriess"></h2>
+    `;
+  });
+
+
+});
