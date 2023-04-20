72d19a32cc163fbaa645a5e34aea4bf70881c608
Lawrence Amoafo <rekenburg777@gmail.com>
2023-04-20 13:40:01 +0000

    Test done to verify no of comment counts

---
+++ src/module/_tests_/countComments.test.js
@@ -0,0 +1,13 @@
+import countComments from '../countComments.js';
+
+test('count items', () => {
+  document.body.innerHTML = `<ul class="comments-lists">
+                                <p>12/03/2021 Lawrence: I love meat</p>
+                                <p>12/05/2023 Kenny: Cool show</p>
+                                <p>12/03/2021 Heaven: Dessert are the best</p>
+                                <p>1/08/2022 Sammy: Nothing beats chicken</p>
+                            </ul>`;
+  const myListOfComments = document.querySelector('.comments-lists');
+  const nbOfComments = countComments(myListOfComments);
+  expect(nbOfComments).toBe(4);
+});
