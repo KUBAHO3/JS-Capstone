cd339e4524faa06c1638744a6f5c6cadd88280dc
Lawrence Amoafo <rekenburg777@gmail.com>
2023-04-20 11:34:05 +0000

    Add function to display the comments
    This include the comment count

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import './style.css';
 import addHeaderToPage from './module/addHeader.js';
 import Modal from './module/displayPopup.js';
 import displayList from './module/displayList.js';
+import displaycomments from './module/displayComments.js';
+import postComments from './module/postComments.js';
+import newComment from './module/newComment.js';
+import countComments from './module/countComments.js';
+import fetchList from './module/fetchList.js';
 
 displayList();
 addHeaderToPage();
@@ -9,6 +14,20 @@ addHeaderToPage();
 const openModal = document.querySelector('.modal');
 const openModalButton = document.querySelector('.container-cards');
 const overlay = document.getElementById('overlay');
+
+const commentUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/KTRp7FI3FBVZYfboByZV/comments';
+const retrieveComment = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/KTRp7FI3FBVZYfboByZV/comments?item_id=';
+
+const loadComments = (id) => {
+  fetchList(`${retrieveComment}${id}`).then((comment) => {
+    const commentList = document.querySelector('.comments-lists');
+    displaycomments(commentList, comment);
+
+    const noOfComment = document.querySelector('.no-of-comments');
+    const commentReturned = countComments(commentList);
+    noOfComment.innerHTML = `(${commentReturned})`;
+  });
+};
 
 const popupWindow = (id) => {
   if (openModal == null) return;
@@ -26,6 +45,7 @@ const closeWindow = () => {
 openModalButton.addEventListener('click', (e) => {
   if (e.target.className === 'card-button') {
     popupWindow(e.target.id);
+    loadComments(e.target.id);
   }
 });
 
@@ -39,5 +59,29 @@ overlay.addEventListener('click', () => {
 openModal.addEventListener('click', (e) => {
   if (e.target.className === 'close-button') {
     closeWindow();
+  }
+});
+
+openModal.addEventListener('click', (e) => {
+  e.preventDefault();
+  const today = new Date();
+  const commentList = document.querySelector('.comments-lists');
+  if (e.target.className === 'received') {
+    const formName = document.querySelector('.name');
+    const comment = document.querySelector('.comment');
+    const form = document.querySelector('form');
+
+    if (!(formName.value === '') || !(comment.value === '')) {
+      postComments(commentUrl, e.target.id, formName.value, comment.value);
+      newComment(commentList, {
+        date: today.toLocaleDateString(),
+        name: formName.value,
+        comment: comment.value,
+      });
+      form.reset();
+      const noOfComment = document.querySelector('.no-of-comments');
+      const commentReturned = countComments(commentList);
+      noOfComment.innerHTML = `(${commentReturned})`;
+    }
   }
 });
